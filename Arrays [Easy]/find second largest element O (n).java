833f5d1f5b6b7a3a9a0b8e6a8b7f558997523a5a
Anuj8611 <2k24.cs1e.2410975@gmail.com>
2025-05-03 13:21:13 +0530

    Update find second largest element O (n).java

--- Arrays [Easy]/find second largest element O (n).java
+++ Arrays [Easy]/find second largest element O (n).java
@@ -1,1 +1,24 @@
 //find second largest element
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Solution {
+    public int secondLargestElement(int[] nums) {
+        int largest=Integer.MIN_VALUE;
+        int secondlargest = -1;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>largest)
+            {
+                secondlargest=largest;
+                largest=nums[i];
+            }
+            
+            else if (nums[i] > secondlargest && nums[i] != largest) {
+                secondlargest = nums[i];
+            }
+        }
+        return secondlargest;
+    }
+    
+}
